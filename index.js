56fcbdd55148aff93b6c1ec91c299b6c2b9bce34
makecode-simple <giadai.official@gmail.com>
2025-03-06 16:03:02 +0700

    Fix lỗi request is not defined

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { getImages } = require("./cloudinaryService");
 const { sendMessage, getChatGPTResponse } = require("./messengerService");
+const request = require("request");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
